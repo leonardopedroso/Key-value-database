9264472a05715bde3c055d09cd1ac4acff935b28
LeonardoPedroso <leonardo.pedroso@tecnico.ulisboa.pt>
2021-06-05 17:11:10 +0100

    KVS lin header ajusted according to fenix file

--- _KVSLib/KVS-lib.h
+++ _KVSLib/KVS-lib.h
@@ -1,27 +1,21 @@
-#ifndef KVS_LIB_H
-#define KVS_LIB_H
-
-#define SUCCESS 0
-#define ERROR_ACCSS_DENIED -1
-#define ERROR_GROUP_DSNT_EXIST -2
-#define ERROR_ALLOC -3
-#define ERROR_KEY_DSNT_EXIST -4
-
-#define ERROR_CREATING_SOCK -100
-#define ERROR_CONNECTION_SERVER -101
-#define ERROR_COM_SERVER -102
-#define ERROR_DISCONNECTED_SOCK -103
-#define ERROR_ALRDY_CONNECTED_SOCK -104
-#define ERROR_CALLBACK_SOCK -105
-#define ERROR_CALLBACK_COM_ERROR -106
-
-#define ERROR_COM_AUTH_SERVER -200
-
+enum KVS_LIB_STATUS{
+SUCCESS = 0,
+ERROR_ACCSS_DENIED = -1,
+ERROR_GROUP_DSNT_EXIST = -2,
+ERROR_ALLOC = -3,
+ERROR_KEY_DSNT_EXIST = -4,
+ERROR_CREATING_SOCK = -100,
+ERROR_CONNECTION_SERVER = -101,
+ERROR_COM_SERVER = -102,
+ERROR_DISCONNECTED_SOCK = -103,
+ERROR_ALRDY_CONNECTED_SOCK = -104,
+ERROR_CALLBACK_SOCK = -105,
+ERROR_CALLBACK_COM_ERROR = -106,
+ERROR_COM_AUTH_SERVER = -200
+};
 int establish_connection (char * group_id, char * secret);
 int put_value(char * key, char * value);
 int get_value(char * key, char ** value);
 int delete_value(char * key);
 int register_callback(char * key, void (*callback_function)(char *));
 int close_connection();
-
-#endif
